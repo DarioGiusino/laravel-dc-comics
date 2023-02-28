c3549f1486726b5fb00ad160f83b71f7a975d063
DarioGiusino <dario.giusino@gmail.com>
2023-02-28 15:23:25 +0100

    imported footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,69 @@
+    {{-- * footer top --}}
+    <section id="footer-top">
+      <div class="container">
+        <div class="footer-list-container">
+          <div class="footer-list-col">
+            <h3>dc comics</h3>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+
+            <h3>shop</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="footer-list-col">
+            <h3>dc</h3>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy policy (New)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent WorkShops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="footer-list-col">
+            <h3>sites</h3>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+        </div>
+        <figure>
+          <img src="{{ asset('images/dc-logo-bg.png') }}" alt="DC logo">
+        </figure>
+      </div>
+    </section>
+
+    {{-- * footer bottom --}}
+    <section id="footer-bottom">
+      <div class="container">
+        <button class="footer-btn">sign-up now!</button>
+        <div class="footer-links">
+          <a href="#">follow us</a>
+          <a href="#"><img src="{{ asset('images/footer-facebook.png') }}" alt="fb"></a>
+          <a href="#"><img src="{{ asset('images/footer-twitter.png') }}" alt="tw"></a>
+          <a href="#"><img src="{{ asset('images/footer-youtube.png') }}" alt="yt"></a>
+          <a href="#"><img src="{{ asset('images/footer-pinterest.png') }}" alt="pt"></a>
+          <a href="#"><img src="{{ asset('images/footer-periscope.png') }}" alt="ps"></a>
+        </div>
+      </div>
+    </section>
