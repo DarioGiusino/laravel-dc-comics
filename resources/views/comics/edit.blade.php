f6bb9204441f60a0ce8f3b6341fa24cdeead47c9
DarioGiusino <dario.giusino@gmail.com>
2023-03-02 15:27:42 +0100

    prefilled fields

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -21,14 +21,17 @@
     {{-- form --}}
     <div class="container">
       <form class="w-100" action="{{ route('comics.store') }}" method="post">
+
+        {{-- ! cross-site request forgery --}}
         @csrf
+
         <div class="row">
           {{-- form title --}}
           <div class="col-6">
             <div class="mb-3">
               <label for="title" class="form-label">Title:</label>
               <input type="text" class="form-control" id="title" name="title" placeholder="Comic title..."
-                value="{{ old('title') }}">
+                value="{{ old('title', $comic->title) }}">
             </div>
           </div>
           {{-- form thumb --}}
@@ -36,22 +39,22 @@
             <div class="mb-3">
               <label for="thumb" class="form-label">Thumb:</label>
               <input type="url" class="form-control" id="thumb" name="thumb" placeholder="Comic thumb..."
-                value="{{ old('thumb') }}">
+                value="{{ old('thumb', $comic->thumb) }}">
             </div>
           </div>
           {{-- form description --}}
           <div class="col-12">
             <div class="mb-3">
               <label for="description" class="form-label">Description:</label>
-              <textarea name="description" id="description" cols="30" class="form-control" placeholder="Comic description...">{{ old('description') }}</textarea>
+              <textarea name="description" id="description" cols="30" class="form-control" placeholder="Comic description...">{{ old('description', $comic->description) }}</textarea>
             </div>
           </div>
           {{-- form price --}}
           <div class="col-2">
             <div class="mb-3">
               <label for="price" class="form-label">Price:</label>
-              <input type="number" step="0.01" min="0.00" max="9999.99" value="{{ old('price') ?? '0.00' }}"
-                class="form-control" id="price" name="price">
+              <input type="number" step="0.01" min="0.00" max="9999.99"
+                value="{{ old('price', $comic->price ?? '0.00') }}" class="form-control" id="price" name="price">
             </div>
           </div>
           {{-- form series --}}
@@ -59,7 +62,7 @@
             <div class="mb-3">
               <label for="series" class="form-label">Series:</label>
               <input type="text" class="form-control" id="series" name="series" placeholder="Comic series..."
-                value="{{ old('series') }}">
+                value="{{ old('series', $comic->series) }}">
             </div>
           </div>
           {{-- form type --}}
@@ -68,8 +71,8 @@
               <label for="type" class="form-label">Type:</label>
               <select class="form-select" id="type" name="type">
                 <option value="" selected>Select one...</option>
-                <option @if (old('type') === 'graphic novel') selected @endif value="graphic novel">Graphic Novel</option>
-                <option @if (old('type') === 'comic book') selected @endif value="comic book">Comic Book</option>
+                <option @if (old('type', $comic->type) === 'graphic novel') selected @endif value="graphic novel">Graphic Novel</option>
+                <option @if (old('type', $comic->type) === 'comic book') selected @endif value="comic book">Comic Book</option>
               </select>
             </div>
           </div>
@@ -78,21 +81,21 @@
             <div class="mb-3">
               <label for="sale_date" class="form-label">Sale date</label>
               <input type="date" class="form-control" id="sale_date" name="sale_date"
-                value="{{ old('sale_date') ?? '1999-01-01' }}">
+                value="{{ old('sale_date', $comic->sale_date ?? '1999-01-01') }}">
             </div>
           </div>
           {{-- form artists --}}
           <div class="col-6">
             <div class="mb-3">
               <label for="artists" class="form-label">Artists:</label>
-              <textarea name="artists" id="artists" cols="30" class="form-control" placeholder="Comic artists...">{{ old('artists') }}</textarea>
+              <textarea name="artists" id="artists" cols="30" class="form-control" placeholder="Comic artists...">{{ old('artists', $comic->artists) }}</textarea>
             </div>
           </div>
           {{-- form writers --}}
           <div class="col-6">
             <div class="mb-3">
               <label for="writers" class="form-label">Writers:</label>
-              <textarea name="writers" id="writers" cols="30" class="form-control" placeholder="Comic writers...">{{ old('writers') }}</textarea>
+              <textarea name="writers" id="writers" cols="30" class="form-control" placeholder="Comic writers...">{{ old('writers', $comic->writers) }}</textarea>
             </div>
           </div>
           {{-- buttons --}}
